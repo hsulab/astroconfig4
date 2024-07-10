8e457a7ed6baa3e3b12828b43192949928dafd52
jyxu <ahcigar@foxmail.com>
2024-07-10 16:21:43 -0400

    update yazi

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,13 +1,37 @@
+-- return {
+--   {
+--     "DreamMaoMao/yazi.nvim",
+--     dependencies = {
+--       "nvim-telescope/telescope.nvim",
+--       "nvim-lua/plenary.nvim",
+--     },
+--
+--     keys = {
+--       { "<leader>gy", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
+--     },
+--   },
+-- }
+
 return {
   {
-    "DreamMaoMao/yazi.nvim",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim",
-    },
-
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
     keys = {
-      { "<leader>gy", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
+      {
+        "<leader>y",
+        function() require("yazi").yazi() end,
+        desc = "Open Yazi",
+      },
+      -- {
+      --   -- Open in the current working directory
+      --   "<leader>cw",
+      --   function() require("yazi").yazi(nil, vim.fn.getcwd()) end,
+      --   desc = "Open the file manager in nvim's working directory",
+      -- },
+    },
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
     },
   },
 }

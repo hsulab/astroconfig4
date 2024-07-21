ba20a90b514a9c09450b7c8c7eaf1c790b2de064
hsulab <ahcigar@foxmail.com>
2024-07-21 00:20:37 -0400

    update statusline

---
+++ lua/plugins/heirline.lua
@@ -0,0 +1,35 @@
+return {
+  "rebelot/heirline.nvim",
+  opts = function(_, opts)
+    local status = require "astroui.status"
+    opts.statusline = { -- statusline
+      hl = { fg = "fg", bg = "bg" },
+      -- status.component.mode {
+      --   mode_text = { padding = { left = 1, right = 1 } },
+      -- }, -- add the mode text
+      status.component.git_branch(),
+      status.component.file_info {
+          -- enable the file_icon and disable the highlighting based on filetype
+          -- filename = { fallback = "Empty" },
+          filename = { modify = ":." },
+          -- disable some of the info
+          filetype = false,
+          file_read_only = false,
+          -- add padding
+          padding = { right = 1 },
+          -- define the section separator
+          surround = { separator = "left", condition = false },
+      },
+      status.component.git_diff(),
+      status.component.diagnostics(),
+      status.component.fill(),
+      status.component.cmd_info(),
+      status.component.fill(),
+      status.component.lsp(),
+      status.component.virtual_env(),
+      status.component.treesitter(),
+      status.component.nav(),
+      -- remove the 2nd mode indicator on the right
+    }
+  end,
+}
